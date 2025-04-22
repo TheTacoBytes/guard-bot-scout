d65cbd38db5ba2f890d979c6ecd533f3daea329d
Humberto Ruiz <thetacobytes@gmail.com>
2025-04-22 19:35:36 -0400

    full code

--- src/gaurd_bot/setup.py
+++ src/gaurd_bot/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import glob
 
 package_name = 'gaurd_bot'
 
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch',
+            glob.glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
